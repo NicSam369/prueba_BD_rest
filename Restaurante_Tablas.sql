5b09a70115bc2a8932e8c670f595d02b46c42a50
NicSam369 <samantha.huayapa@ucsp.edu.pe>
2026-06-28 21:37:55 -0500

    Configuracion inicial del backend con flask y PostgreSQL

--- Restaurante_Tablas.sql
+++ Restaurante_Tablas.sql
@@ -1,3 +1,11 @@
+--15
+CREATE TABLE SUCURSAL (
+    id_scrsal SERIAL PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    direccion VARCHAR(150) NOT NULL,
+    ciudad VARCHAR(50) NOT NULL,
+    telefono VARCHAR(20)
+);
 --1
   CREATE TABLE CLIENTE (
 	id_cliente SERIAL PRIMARY KEY,
@@ -8,70 +16,6 @@
 	telefono VARCHAR(20),
 	fecha_registro DATE DEFAULT CURRENT_TIMESTAMP
   );
-
---2
-  CREATE TABLE MESA (
-	id_mesa SERIAL PRIMARY KEY,
-    id_scrsal INT NOT NULL,
-    numero INTEGER NOT NULL,
-    capacidad INTEGER NOT NULL,
-    estado VARCHAR(20) DEFAULT 'Disponible',
-    CONSTRAINT fk_mesa_sucursal
-        FOREIGN KEY (id_scrsal)
-        REFERENCES SUCURSAL(id_scrsal)
-  );
-
---3
-  CREATE TABLE PEDIDO (
-	id_pedido SERIAL PRIMARY KEY,
-    id_cliente INT NOT NULL,          
-    id_empleado INT NOT NULL,          
-    id_mesa INT NOT NULL,              
-    fecha_hora TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    estado VARCHAR(30) DEFAULT 'Pendiente',
-    total NUMERIC(10,2),
-    CONSTRAINT fk_pedido_cliente
-        FOREIGN KEY (id_cliente)
-        REFERENCES CLIENTE(id_cliente),
-    CONSTRAINT fk_pedido_empleado
-        FOREIGN KEY (id_empleado)
-        REFERENCES EMPLEADO(id_empleado),
-    CONSTRAINT fk_pedido_mesa
-        FOREIGN KEY (id_mesa)
-        REFERENCES MESA(id_mesa)
-  );
-
---4
-  CREATE TABLE DETALLE_PEDIDO (
-	id_detalle SERIAL PRIMARY KEY,
-    id_pedido INT NOT NULL,            
-    id_producto INT NOT NULL,          
-    cantidad NUMERIC(8,0) NOT NULL,
-    precio_unitario NUMERIC(12,2) NOT NULL,
-    subtotal NUMERIC(12,2),
-    CONSTRAINT fk_detalle_pedido_pedido
-        FOREIGN KEY (id_pedido)
-        REFERENCES PEDIDO(id_pedido),
-    CONSTRAINT fk_detalle_pedido_producto
-        FOREIGN KEY (id_producto)
-        REFERENCES PRODUCTO(id_producto)
-  );
-
---5
-CREATE TABLE PRODUCTO (
-	id_producto SERIAL PRIMARY KEY,
-	nombre VARCHAR(50) NOT NULL,
-	descripcion VARCHAR(50),
-	precio DECIMAL(10,2) NOT NULL,
-	stock INT NOT NULL,
-	id_categoria INT NOT NULL,
-
-	CONSTRAINT fk_id_categoria
-		FOREIGN KEY (id_categoria)
-		REFERENCES CATEGORIA(id_categoria)
-		ON DELETE SET NULL
-  );
-
 --6
 CREATE TABLE CATEGORIA (
 	id_categoria SERIAL PRIMARY KEY,
@@ -97,6 +41,17 @@ CREATE TABLE CATEGORIA (
 	  	ON DELETE SET NULL
   );
 
+--2
+  CREATE TABLE MESA (
+	id_mesa SERIAL PRIMARY KEY,
+    id_scrsal INT NOT NULL,
+    numero INTEGER NOT NULL,
+    capacidad INTEGER NOT NULL,
+    estado VARCHAR(20) DEFAULT 'Disponible',
+    CONSTRAINT fk_mesa_sucursal
+        FOREIGN KEY (id_scrsal)
+        REFERENCES SUCURSAL(id_scrsal)
+  );
 --8
   CREATE TABLE PROVEEDOR (
 	id_prvdor SERIAL PRIMARY KEY,
@@ -110,6 +65,59 @@ CREATE TABLE CATEGORIA (
 		FOREIGN KEY (id_scrsal)
 		REFERENCES SUCURSAL(id_scrsal)
   );
+
+--3
+  CREATE TABLE PEDIDO (
+	id_pedido SERIAL PRIMARY KEY,
+    id_cliente INT NOT NULL,          
+    id_empleado INT NOT NULL,          
+    id_mesa INT NOT NULL,              
+    fecha_hora TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    estado VARCHAR(30) DEFAULT 'Pendiente',
+    total NUMERIC(10,2),
+    CONSTRAINT fk_pedido_cliente
+        FOREIGN KEY (id_cliente)
+        REFERENCES CLIENTE(id_cliente),
+    CONSTRAINT fk_pedido_empleado
+        FOREIGN KEY (id_empleado)
+        REFERENCES EMPLEADO(id_empleado),
+    CONSTRAINT fk_pedido_mesa
+        FOREIGN KEY (id_mesa)
+        REFERENCES MESA(id_mesa)
+  );
+--5
+CREATE TABLE PRODUCTO (
+	id_producto SERIAL PRIMARY KEY,
+	nombre VARCHAR(50) NOT NULL,
+	descripcion VARCHAR(50),
+	precio DECIMAL(10,2) NOT NULL,
+	stock INT NOT NULL,
+	id_categoria INT NOT NULL,
+
+	CONSTRAINT fk_id_categoria
+		FOREIGN KEY (id_categoria)
+		REFERENCES CATEGORIA(id_categoria)
+		ON DELETE SET NULL
+  );
+
+--4
+  CREATE TABLE DETALLE_PEDIDO (
+	id_detalle SERIAL PRIMARY KEY,
+    id_pedido INT NOT NULL,            
+    id_producto INT NOT NULL,          
+    cantidad NUMERIC(8,0) NOT NULL,
+    precio_unitario NUMERIC(12,2) NOT NULL,
+    subtotal NUMERIC(12,2),
+    CONSTRAINT fk_detalle_pedido_pedido
+        FOREIGN KEY (id_pedido)
+        REFERENCES PEDIDO(id_pedido),
+    CONSTRAINT fk_detalle_pedido_producto
+        FOREIGN KEY (id_producto)
+        REFERENCES PRODUCTO(id_producto)
+  );
+
+
+
 
 --9
   CREATE TABLE COMPRA (
@@ -211,11 +219,4 @@ CREATE TABLE PROMOCION (
         REFERENCES SUCURSAL(id_scrsal)
 );
 
---15
-CREATE TABLE SUCURSAL (
-    id_scrsal SERIAL PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL,
-    direccion VARCHAR(150) NOT NULL,
-    ciudad VARCHAR(50) NOT NULL,
-    telefono VARCHAR(20)
-);
+
---
+++ backend/app.py
@@ -0,0 +1,40 @@
+from flask import Flask
+from flask_cors import CORS
+from database import get_connection
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def inicio():
+    return {
+        "mensaje": "API RESTAURANTE funcionando correctamente",
+        "estado": "OK"
+    }
+
+@app.route("/api/test-db")
+def test_db():
+    try:
+        conexion = get_connection()
+        cursor = conexion.cursor()
+
+        cursor.execute("SELECT NOW();")
+        fecha = cursor.fetchone()
+
+        cursor.close()
+        conexion.close()
+
+        return {
+            "estado": "OK",
+            "mensaje": "Conexión exitosa a PostgreSQL",
+            "servidor": str(fecha[0])
+        }
+
+    except Exception as e:
+        return {
+            "estado": "ERROR",
+            "mensaje": str(e)
+        }, 500
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
